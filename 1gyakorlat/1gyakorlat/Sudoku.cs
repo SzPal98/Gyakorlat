ab45484fb2e77fc06ca808f377a617d649a83081
SzPal98 <sztrepka98@gmail.com>
2021-10-22 10:04:03 +0200

    Initial commit

---
+++ 1gyakorlat/1gyakorlat/Sudoku.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1gyakorlat
+{
+    class Sudoku
+    {
+        public string Quiz { get; set; }
+        public string Solution { get; set; }
+
+        
+    }
+}
