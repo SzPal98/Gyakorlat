2e948182fd726e6de9aca2fef1232c914ac31236
SzPal98 <sztrepka98@gmail.com>
2021-11-12 10:10:39 +0100

    Labda

---
+++ Mikulas/Mikulas/Entities/labda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mikulas.Entities
+{
+    class labda: Label
+    {
+        public labda()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            var ecset = new SolidBrush(Color.Blue);
+            g.FillEllipse(ecset, 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
