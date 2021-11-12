1d76f92a6bcfb7bdd8f2456225a339ee8200b674
SzPal98 <sztrepka98@gmail.com>
2021-11-12 10:31:15 +0100

    Futoszalag

--- Mikulas/Mikulas/Form1.Designer.cs
+++ Mikulas/Mikulas/Form1.Designer.cs
@@ -30,12 +30,47 @@ namespace Mikulas
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CreateTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 349);
+            this.panel1.TabIndex = 0;
+            // 
+            // CreateTimer
+            // 
+            this.CreateTimer.Enabled = true;
+            this.CreateTimer.Interval = 3000;
+            this.CreateTimer.Tick += new System.EventHandler(this.CreateTimer_Tick);
+            // 
+            // conveyorTimer
+            // 
+            this.conveyorTimer.Enabled = true;
+            this.conveyorTimer.Interval = 10;
+            this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer CreateTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
     }
 }
 
