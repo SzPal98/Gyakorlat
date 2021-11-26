b36260cd47a05b8ed4f93eb7f59d1ee7d030a77a
SzPal98 <sztrepka98@gmail.com>
2021-11-26 10:34:13 +0100

    Validalas fuggvenyek

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [
+            Test,
+            TestCase("abcd", "false"),
+            TestCase("abcd@gmail.com", "true")
+            
+            
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
